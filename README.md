5fd316c909cd61f751390c5da44b4892d5b9850b
tfqhkl <taufiqhaikal1808@gmail.com>
2026-08-27 13:43:59 +0800

    initial + moveable circle

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# doppler-effect
+# <p align="center"> **Doppler Effect Simulator.** </p>
+
+#### overview.
+This projects serves as a practice for C's [raylib](https://www.raylib.com/) and using CMake.
+
+#### my setup.
+1. NixOS 25.06.
+2. Neovim v0.12.3 (via Nixvim).
+2. [Nix Shell via `flake.nix`.](./flake.nix)
+
+#### compile using `gcc`.
+```
+gcc -Wall -Wextra -o doppler doppler.c -lraylib
+```
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "c packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ # base tools.
+          gcc
+          cmake
+          ninja
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [ # extras (header files, etc.)
+          raylib
+        ];
+
+        shellHook = ''
+          export CC=gcc
+        '';
+      };
+    };
+}
